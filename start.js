717da38621f9daff2fe5565b425116c37128f4bf
Alex Merkert <alex.merkert@gmail.com>
2020-11-03 17:04:05 -0500

    fixing a thing for winders

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 const server = express();
 const port = 4000;
 
@@ -11,7 +11,15 @@ server.use(express.static('.'));
 
 server.listen(port, () => {
   console.log(`Server listening at ${port}`);
-  spawn('open', ['http://localhost:4000']);
+
+  switch (process.platform) {
+    case 'win32':
+      exec('start http://localhost:4000');
+      break;
+    case 'darwin':
+      exec('open http://localhost:4000');
+      break;
+  }
 });
 
 process.on('exit', () => {
