82e820167194cd53a3b26d9e2d67ba761f3ff1d8
Alex Merkert <alex.merkert@gmail.com>
2022-01-25 17:42:16 -0500

    make scrollable grid canvas

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ let _modules = {};
 let _imageCache = [];
 let _audioCache = [];
 
+let _offset = { x: 0, y: 0 };
+
+let _cursor = null;
+const _scrollCanvas = {
+  left: false,
+  right: false,
+  up: false,
+  down: false,
+  scrolling: () => _scrollCanvas.left || _scrollCanvas.right || _scrollCanvas.up || _scrollCanvas.down,
+};
+
+const SCROLL_SPEED = 1 / 3;
+const GRID_SIZE = 64;
+const GRID_COLOR = '#555';
+const CURSOR_COLOR = '#0ff';
+let drawGrid = true;
+
 window.addEventListener('keydown', event => {
   if (_keys[event.key] === undefined || _keys[event.key] === -1) {
     _keys[event.key] = _frame;
@@ -110,14 +127,56 @@ async function saveRun() {
 }
 
 function setupCanvasClickHandler() {
-  document.querySelector('canvas').addEventListener('mousedown', event => {
+  const canvas = document.querySelector('canvas');
+
+  canvas.addEventListener('mousedown', event => {
     const { x, y } = event;
     if (clickFunction) clickFunction(_game, { x, y });
+    
+    const gridStartX = Math.floor(_offset.x / GRID_SIZE) * GRID_SIZE;
+    const gridStartY = Math.floor(_offset.y / GRID_SIZE) * GRID_SIZE;
+    console.log('GRID START', gridStartX, gridStartY);
+  });
+
+  canvas.addEventListener('mousemove', event => {
+    const { offsetX: x, offsetY: y } = event;
+    const { height, width } = canvas.getBoundingClientRect();
+    const scaleH = canvas.height / height;
+    const scaleW = canvas.width / width;
+    const gridScaled = GRID_SIZE / scaleH; // scaleW and scaleH _should_ be the same
+    
+    _scrollCanvas.left = x < gridScaled;
+    _scrollCanvas.right = x > width - gridScaled;
+    _scrollCanvas.up = y < gridScaled;
+    _scrollCanvas.down = y > height - gridScaled;
+
+    if (!_scrollCanvas.scrolling()) {
+      const canvasX = x * scaleW;
+      const canvasY = y * scaleH;
+      _cursor = { x: canvasX, y: canvasY };
+    } else {
+      _cursor = null;
+    }
+  });
+
+  canvas.addEventListener('mouseout', event => {
+    _scrollCanvas.left = false;
+    _scrollCanvas.right = false;
+    _scrollCanvas.up = false;
+    _scrollCanvas.down = false;
+    _cursor = null;
   });
 }
 
 function update() {
   try {
+    if (_scrollCanvas.scrolling()) {
+      const scrollAmt = GRID_SIZE * SCROLL_SPEED;
+      if (_scrollCanvas.left) _offset.x -= scrollAmt;
+      else if (_scrollCanvas.right) _offset.x += scrollAmt;
+      if (_scrollCanvas.up) _offset.y -= scrollAmt;
+      else if (_scrollCanvas.down) _offset.y += scrollAmt;
+    }
     if (updateFunction) updateFunction(_game, _frame, { ..._keys });
     _frame++;
     // remove released keys
@@ -136,6 +195,50 @@ function draw() {
     if (canvas) {
       const context = canvas.getContext('2d');
       context.clearRect(0, 0, canvas.width, canvas.height);
+
+      context.save();
+      context.translate(-_offset.x, -_offset.y);
+
+      if (drawGrid) {
+        const gridStartX = Math.floor(_offset.x / GRID_SIZE) * GRID_SIZE;
+        const gridStartY = Math.floor(_offset.y / GRID_SIZE) * GRID_SIZE;
+        
+        context.fillStyle = '#501';
+        context.fillRect(GRID_SIZE, canvas.height - (3 * GRID_SIZE), GRID_SIZE, GRID_SIZE);
+
+        context.save();
+        context.lineWidth = 1;
+        context.strokeStyle = GRID_COLOR;
+        for(let h = gridStartX; h <= canvas.width + gridStartX + GRID_SIZE; h += GRID_SIZE) {
+          context.beginPath();
+          context.moveTo(h, gridStartY);
+          context.lineTo(h, canvas.height + gridStartY + GRID_SIZE);
+          context.stroke();
+          context.closePath();
+        }
+        for(let w = gridStartY; w <= canvas.height + gridStartY + GRID_SIZE; w += GRID_SIZE) {
+          context.beginPath();
+          context.moveTo(gridStartX, w);
+          context.lineTo(canvas.width + gridStartX + GRID_SIZE, w);
+          context.stroke();
+          context.closePath();
+        }
+
+        if (_cursor) {
+          context.save();
+          context.globalAlpha = 0.3;
+          const { x, y } = _cursor;
+          const cellX = Math.floor((x + _offset.x) / GRID_SIZE);
+          const cellY = Math.floor((y + _offset.y) / GRID_SIZE);
+          context.fillStyle = CURSOR_COLOR;
+          context.fillRect(cellX * GRID_SIZE, cellY * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+          context.restore();
+        }
+        context.restore();
+      } 
+
+      context.restore();
+
       if (drawFunction) drawFunction(_game, _images, _frame, context, canvas);
     }
   } catch (ex) { console.error(ex); }
